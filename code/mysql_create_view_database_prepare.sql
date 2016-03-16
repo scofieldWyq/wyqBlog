7af82eb989651a4c3e930bd9e5242def871f2cf5
wuyingqiang <147036scofield@gmail.com>
2016-03-16 16:14:48 +0800

    complete file `mysql_create_view_database_prapare.sql`

---
+++ code/mysql_create_view_database_prepare.sql
@@ -0,0 +1,63 @@
+/* This is for create view of mysql testing
+
+   1. in the empty database
+   2. craete two tables
+   3. start to insert some data for testing.
+   */
+
+/* create the test database */
+CREATE DATABASE IF NOT EXISTS dbForViewTest;
+
+/* change into dbForViewTest database */
+USE dbForViewTest;
+
+/* craete table for testing*/
+-- drop the table if exists.
+DROP TABLE IF EXISTS teacher;
+DROP TABLE IF EXISTS student;
+
+
+-- create table
+CREATE TABLE student (
+  sno VARCHAR(20),
+  sname VARCHAR(20),
+  tno VARCHAR(20),
+  PRIMARY KEY sno,
+  CONSTRAINT FOREIGN KEY (tno) REFERENCES teacher(tno) ON DELETE CASCADE
+);
+
+CREATE TABLE teacher (
+  tno VARCHAR(20),
+  tname VARCHAR(20),
+  PRIMARY KEY tno,
+);
+
+/* insert teacher data */
+INSERT INTO teacher(tno tname) VALUES(10000, wa);
+INSERT INTO teacher(tno tname) VALUES(10001, wb);
+INSERT INTO teacher(tno tname) VALUES(10002, wc);
+INSERT INTO teacher(tno tname) VALUES(10003, wd);
+INSERT INTO teacher(tno tname) VALUES(10004, we);
+INSERT INTO teacher(tno tname) VALUES(10005, wf);
+INSERT INTO teacher(tno tname) VALUES(10006, wg);
+INSERT INTO teacher(tno tname) VALUES(10007, wh);
+INSERT INTO teacher(tno tname) VALUES(10008, wi);
+INSERT INTO teacher(tno tname) VALUES(10009, wj);
+
+/* insert student data */
+INSERT INTO student(sno sname, tname) VALUES(20000, sa, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20001, sb, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20002, sc, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20003, sd, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20004, se, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20005, sf, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20006, sg, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20007, sh, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20008, si, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20009, sj, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20010, sk, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20011, sl, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20012, sm, 10000);
+INSERT INTO student(sno sname, tname) VALUES(20013, sn, 10000);
+
+/* end */
